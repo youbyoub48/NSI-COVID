11f7a6da979b4a07c4ca93f9cc9fc85781e9e765
youbyoub48 <ayou8@hotmail.fr>
2021-04-28 01:32:57 +0200

    création de bouton

--- src/covid/views.py
+++ src/covid/views.py
@@ -6,4 +6,5 @@ import API
 def covid(request, Pays="France", mode="deaths", jours=30):
     days, data = API.Multi_Pays(pays=Pays.split(","), mode=mode, jours=jours)
     jours_label = {7: "Semaine", 30: "Mois", 365: "Année"}.get(jours, "Personnalisé")
-    return render(request, "covid/dashboard.html", context={"Data": data, "days_labels": days, "Mode":mode, "jours_label": jours_label, "Pays":Pays})
+    mode_fr = {"deaths": "Morts", "confirmed": "Cas Confirmé", "recovered": "Guéris"}.get(mode)
+    return render(request, "covid/dashboard.html", context={"Data": data, "days_labels": days, "mode_fr":mode_fr, "jours_label": jours_label, "Pays":Pays, "Mode":mode, "jours":jours})
