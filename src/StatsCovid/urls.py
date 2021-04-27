c96ce8283c10a3389c2fab892952995c7ef3a7a1
youbyoub48 <ayou8@hotmail.fr>
2021-04-27 13:42:10 +0200

    site

--- src/StatsCovid/urls.py
+++ src/StatsCovid/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from covid.views import covid
 
 urlpatterns = [
+    path('', covid),
     path('admin/', admin.site.urls),
 ]
---
+++ src/covid/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def covid(request):
+    return render(request, "covid/stats.html")
