90f75f9e3215c72fe054a6ece73cae91bb564e2e
youbyoub48 <ayou8@hotmail.fr>
2021-04-27 11:08:41 +0200

    Création de l'api

--- API.py
+++ API.py
@@ -1,1 +1,29 @@
+from datetime import date, timedelta
 import requests
+import json
+
+def Covid_Pays(pays, mode, jours=30):
+    debut_date = date.today()
+    fin_date = debut_date - timedelta(days=jours)
+    get = requests.get(f" https://api.covid19api.com/total/country/{pays}/status/{mode}?from={fin_date}T00:00:00Z&to={debut_date}T00:00:00Z ")
+    if not get and not get.json():
+        return False, False
+    get = get.json()
+    api_data = {}
+    for i in range(jours):
+        Date = get[i]["Date"]
+        Date = Date[:10]
+        Cases = get[i]["Cases"]
+        api_data[Date] = Cases
+    return api_data
+
+
+def Multi_Pays(pays, mode, jours=7):
+    for line in pays:
+        print(json.dumps(Covid_Pays(pays=line,mode=mode,jours=jours), indent=4))
+    
+
+
+if __name__ == "__main__":
+    #print(json.dumps(Covid_Pays("france","confirmed",jours=7), indent=4))
+    Multi_Pays(pays=["France","South-Africa"],mode="confirmed")
